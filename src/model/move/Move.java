6e42d28485d653d90ec33602e4736d78ee016174
gianlucamonica <gianluca.monica@studenti.unipr.it>
2018-03-12 12:28:26 +0100

    aggiunti commenti in Card.java, Move.java

--- src/model/move/Move.java
+++ src/model/move/Move.java
@@ -2,20 +2,30 @@ package model.move;
 
 import model.card.Card;
 
-// Mossa fatta da un giocatore
+/**
+Class representing the game move
+*/
 public class Move{
 
-	private Card card1;
-	private Card card2;
-	private boolean match = false;
+	private Card card1; /** first card of the move */
+	private Card card2;  /** second card of the move */
+	private boolean match = false; /** match between the previous two cards */
 
-	// quando il giocatore scopre la prima carta
+	/**
+     * @desc move constructor, when the player moves the first card
+     * @param card $card1
+     * @return void
+   */
 	public Move(Card card1) {
 		this.card1 = card1;
 		this.card2 = null;
 	}
 
-	//quando il giocatore scopre due carte
+	/**
+     * @desc move constructor, when the player moves both the cards
+     * @param card $card1, card $card2
+     * @return void
+   */
 	public Move(Card card1, Card card2) {
 		this.card1 = card1;
 		this.card2 = card2;
@@ -23,25 +33,50 @@ public class Move{
 			this.match = true;
 		}
 	}
-
+	
+	/**
+     * @desc check if two cards match or not setting match 
+     * @param card $card1, card $card2
+     * @return void
+   */
 	public void match(Card card1, Card card2) {
 		if (card1.getValue() == card2.getValue()){
 			this.match = true;
 		}
 	}
-
+	
+	/**
+     * @desc get the first card
+     * @param 
+     * @return card $card1
+   */
 	public Card getCard1() {
 		return card1;
 	}
 
+	/**
+     * @desc set the first card
+     * @param card $card1
+     * @return void
+   */
 	public void setCard1(Card card1) {
 		this.card1 = card1;
 	}
-
+	
+	/**
+     * @desc get the second card
+     * @param 
+     * @return card $card2
+   */
 	public Card getCard2() {
 		return card2;
 	}
 
+	/**
+     * @desc set the second card
+     * @param card $card2
+     * @return void
+   */
 	public void setCard2(Card card2) {
 		this.card2 = card2;
 	}
