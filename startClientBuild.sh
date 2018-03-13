3f41372b7fddfc92ba442444cbfd6a9a6526840d
Alice Graziosi <alice.graziosi@labs.cs.unibo.it>
2018-03-13 16:27:59 +0100

    script linux

---
+++ startClientBuild.sh
@@ -0,0 +1,2 @@
+java -classpath out/production/memory ClientGiocatore
+cmd /k
---
+++ startServerBuild.sh
@@ -0,0 +1,1 @@
+java -classpath out/production/memory -Djava.rmi.server.codebase=file:out/production/memory RegistrationServiceServer 10 &
